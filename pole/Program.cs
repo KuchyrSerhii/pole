73facddbfe58463503737f241c2130e535cfcefa
ou7pc18 <ou7pc18@OU7PC18>
2022-12-15 11:43:46 +0100

    Přidejte soubory projektu.

---
+++ pole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] simpsonovi = { "Homer", "Marge", "Bart", "Lisa", "Maggie" };
+            Array.Sort(simpsonovi);
+            foreach (string serazeno in simpsonovi)
+                Console.WriteLine("{0}", serazeno);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
